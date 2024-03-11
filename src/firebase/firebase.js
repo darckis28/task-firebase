9ee23c3137354dfc3b07b06c1c89b110fe20dd44
Andi Aguirre <darckisgamer@gmail.com>
2024-03-10 21:15:31 -0500

    add varibles de entorno

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCo5e0f8xv3-kTB1hXde_L63kb2_jUGog8",
-  authDomain: "task-firebase-fd202.firebaseapp.com",
-  projectId: "task-firebase-fd202",
-  storageBucket: "task-firebase-fd202.appspot.com",
-  messagingSenderId: "9325960097",
-  appId: "1:9325960097:web:aa990b5c97bac600c689e0",
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.VITE_AUTH,
+  projectId: import.meta.env.VITE_PROYECT_ID,
+  storageBucket: import.meta.env.VITE_STORE,
+  messagingSenderId: import.meta.env.VITE_MESSA,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
